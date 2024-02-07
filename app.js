a4a657257792b426f5ae51b8f45a634be3ba0634
Dinoplant <143848581+Dinoplant@users.noreply.github.com>
2024-02-07 14:16:10 -0800

    IDK fr

--- app.js
+++ app.js
@@ -9,23 +9,49 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // optionally update when fields update
     // tbd
+    document.getElementById(`name`).addEventListener(`change`, function (eventData){
+      let nameOutput = document.getElementById(`nameOutput`);
+      // console.log(eventData);
+      nameOutput.innerText = eventData.target.value;
+    })
+
+    document.getElementById(`type`).addEventListener(`change`, function (eventData){
+      let nameOutput = document.getElementById(`nameOutput`);
+      let info = document.getElementById(`input`)
+      info.innerText = formData.name
+    })
+
 
     // process form Data
     document.getElementById("myForm").addEventListener("submit", function (e) {
-        e.preventDefault();
+        e.preventDefault(); 
       
         var formData = new FormData(form);
-        // output as an object
-        console.log(Object.fromEntries(formData));
-      
-        // ...or iterate through the name-value pairs
-        for (var pair of formData.entries()) {
-          console.log(pair[0] + ": " + pair[1]);
-        }
+        console.log(eventdata.target)
+        var formData = new FormData(e.target);
+        formData = Object.fromEntries(eventdata.target);
+
+
+
+        //Update certifctae 
+        let nameOutput = document.getElementById(`nameOutput`);
+        let info = document.getElementById(`input`);
+        nameOutput.inner = formData.name;
+        info.innerText = formData.type;
+
+
+
+
+
+        
       });
     // Log readiness to console
     console.log("Ready");
+
+    // use visible to to and make it none to hide things
 });
+
+
 
 function processForm(form){
 
