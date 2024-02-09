f123d7c8d9d86db1e56f99d1066acf050f796a47
Dinoplant <angel.delgado_jimenez23@student.cart.com>
2024-02-08 22:40:34 -0800

    I have no clue what I did

--- app.js
+++ app.js
@@ -1,58 +1,80 @@
 // Add Event listners when DOM is ready
 document.addEventListener("DOMContentLoaded", function () {
 
-    // check for buttons exist
-    if ( document.querySelectorAll('.button') !== null) {
-        let buttons = document.querySelectorAll('.button');
-        buttons.forEach(button => button.addEventListener('click', processForm))
-    }
+  // check for buttons exist
+  // if (document.querySelectorAll('.button') !== null) {
+  //   let buttons = document.querySelectorAll('.button');
+  //   buttons.forEach(button => button.addEventListener('click', processForm))
+  // }
 
-    // optionally update when fields update
-    // tbd
-    document.getElementById(`name`).addEventListener(`change`, function (eventData){
-      let nameOutput = document.getElementById(`nameOutput`);
-      // console.log(eventData);
-      nameOutput.innerText = eventData.target.value;
-    })
+  // optionally update when fields update
+  // tbd
+  document.getElementById('name').addEventListener('change', function (eventData) {
+    let nameOutput = document.getElementById('nameOutput');
+    console.log(nameOutput);
+    nameOutput.innerText = eventData.target.value;
+  });
 
-    document.getElementById(`type`).addEventListener(`change`, function (eventData){
-      let nameOutput = document.getElementById(`nameOutput`);
-      let info = document.getElementById(`input`)
-      info.innerText = formData.name
-    })
+  document.getElementById('type').addEventListener('change', function (eventData) {
+    let nameOutput = document.getElementById('nameOutput');
+    console.log(eventData);
+    let info = document.getElementById('info');
+    info.innerText = eventData.target.value;
+    changeCert(eventData.target.value);
+  });
 
+  // process form Data
+  // document.getElementById("myForm").addEventListener("submit", function (eventData) {
+  //   eventData.preventDefault(); //stop page reload when form is submitted
+  //   console.log(eventData.target);
+  //   var formData = new FormData(eventData.target);
+  //   formData = Object.fromEntries(formData);
 
-    // process form Data
-    document.getElementById("myForm").addEventListener("submit", function (e) {
-        e.preventDefault(); 
-      
-        var formData = new FormData(form);
-        console.log(eventdata.target)
-        var formData = new FormData(e.target);
-        formData = Object.fromEntries(eventdata.target);
+  //   //update certificate information when form is submitted:
+  //   let nameOutput = document.getElementById('nameOutput');
+  //   let info = document.getElementById('info');
+  //   nameOutput.innerText = formData.name;
+  //   info.innerText = formData.type;
 
+  // });
+  // Log readiness to console
+  // console.log("Ready");
 
-
-        //Update certifctae 
-        let nameOutput = document.getElementById(`nameOutput`);
-        let info = document.getElementById(`input`);
-        nameOutput.inner = formData.name;
-        info.innerText = formData.type;
-
-
-
-
-
-        
-      });
-    // Log readiness to console
-    console.log("Ready");
-
-    // use visible to to and make it none to hide things
-});
+  // let info = document.getElementById('info');
+  // info.classList.add('test');
 
 
 
-function processForm(form){
+  // certificate.classList.add('number1');
 
-}
+//   function changeCert(value) {
+//     let certificate = document.getElementById('certificate');
+//     certificate.classList = ""; // clear classes on each function call
+//     switch (value) {
+//       case 'A':
+//         certificate.classList.add('number1');
+//         break;
+//       case 'B':
+//         certificate.classList.add('number2');
+//         break;
+//       case 'C':
+//         certificate.classList.add('number3');
+//         break;
+//       case 'D':
+//         certificate.classList.add('number4');
+//         break;
+//     }
+//   }
+// });
+
+/* Additional things to be aware of */
+
+
+
+
+
+
+//  function processForm(form) {
+
+// }
+})
